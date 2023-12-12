793b06e7667d817ff0e56e09affc83c76b245cbf
woooyas <njmin8@naver.com>
2023-12-12 17:26:43 +0900

    refactor: 인터페이스 단순화 및 클래스 자바독 추가

--- src/main/java/com/nhnacademy/aiot/broker/MODBUSBroker.java
+++ src/main/java/com/nhnacademy/aiot/broker/MODBUSBroker.java
@@ -1,18 +1,19 @@
 package com.nhnacademy.aiot.broker;
 
-public interface MODBUSBroker {
-    /**
-     * BufferedInputStream이 byte-input stream으로부터 주어진 offset을 시작으로 byte 배열의 byte를
-     * 읽어오도록 합니다.
-     * 
-     * @param b   읽어올 버퍼
-     * @param off byte를 저장하기 시작할 오프셋
-     * @param len 읽어올 byte의 최대 길이
-     */
-    public void read(byte[] b, int off, int len);
+/*
+ * Modbus 클라이언트 간의 메세지를 조정하는 클래스입니다.
+ */
+public interface ModbusBroker {
 
     /**
-     * BufferedOutputStream이 output stream에 byte 배열의 length만큼 byte를 쓰도록 합니다.
+     * input stream에서 읽어온 byte 배열을 반환합니다.
+     * 
+     * @return 읽어온 데이터
+     */
+    public byte[] read();
+
+    /**
+     * output stream에 byte 배열의 length만큼 byte를 쓰도록 합니다.
      * 
      * @param b write할 데이터
      */
