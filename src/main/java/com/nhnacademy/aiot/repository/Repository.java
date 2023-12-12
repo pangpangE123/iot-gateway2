17a01129faa73b9849d479f3596449071376e7b7
Jeongyong Lee <hua9378@gmail.com>
2023-12-12 17:51:37 +0900

    docs: 자바독 추가

--- src/main/java/com/nhnacademy/aiot/repository/Repository.java
+++ src/main/java/com/nhnacademy/aiot/repository/Repository.java
@@ -1,12 +1,47 @@
 package com.nhnacademy.aiot.repository;
 
+/**
+ * 영속성 관리를 위한 인터페이스를 제공하는 저장소 클래스입니다.
+ * <p>
+ * 제네릭으로 저장할 데이터의 타입을 명시합니다.
+ */
 public interface Repository<T> {
 
-    void save(T t);
+    /**
+     * 객체를 저장합니다.
+     * 
+     * @param t 저장할 객체입니다.
+     * @return 객체를 저장하면 <code>true</code>를 반환합니다.
+     *         <p>
+     *         객체를 저장하지 못하면 <code>false</code>를 반환합니다.
+     */
+    boolean save(T t);
 
+    /**
+     * 아이디를 통해 저장된 객체를 찾습니다.
+     * 
+     * @param id 찾을 객체의 아이디입니다.
+     * @return 찾은 객체를 반환합니다.
+     */
     T findById(String id);
 
-    void remove(T t);
+    /**
+     * 저장된 객체를 지웁니다.
+     * 
+     * @param t 지울 객체입니다.
+     * @return 객체를 지우면 <code>true</code>를 반환합니다.
+     *         <p>
+     *         객체를 지우지 못하면 <code>false</code>를 반환합니다.
+     */
+    boolean remove(T t);
 
-    void remove(String id);
+    /**
+     * 저장된 객체를 지웁니다.
+     * 
+     * @param id 지울 객체의 아이디입니다.
+     * @return 객체를 지우면 <code>true</code>를 반환합니다.
+     *         <p>
+     *         객체를 지우지 못하면 <code>false</code>를 반환합니다.
+     */
+    boolean remove(String id);
 }
