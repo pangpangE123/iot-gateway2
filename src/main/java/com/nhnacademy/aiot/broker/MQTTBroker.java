9559867b335a6dad229f48af255471b100fdf987
woooyas <njmin8@naver.com>
2023-12-12 16:39:27 +0900

    feat: MQTTBroker 추가

---
+++ src/main/java/com/nhnacademy/aiot/broker/MQTTBroker.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.aiot.broker;
+
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public interface MQTTBroker {
+    /**
+     * MQTTCLient가 subscribe하도록 합니다.
+     * 
+     * @param topicFilter     subscribe할 topic (application name)
+     * @param messageListener 수신된 메세지를 처리할 callback
+     */
+    public void subscribe(String topicFilter, IMqttActionListener messageListener);
+
+    /**
+     * MQTTClient가 publish하도록 합니다.
+     * 
+     * @param topic   메세지를 보낼 topic
+     * @param message 보낼 메세지
+     */
+    public void publish(String topic, MqttMessage message);
+}
