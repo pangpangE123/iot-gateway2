a0071aa972d070ed648690315e4686fcaae29b60
woooyas <njmin8@naver.com>
2023-12-12 17:27:47 +0900

    docs: 자바독 추가

--- src/main/java/com/nhnacademy/aiot/broker/MQTTBroker.java
+++ src/main/java/com/nhnacademy/aiot/broker/MQTTBroker.java
@@ -3,6 +3,9 @@ package com.nhnacademy.aiot.broker;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+/*
+ * MQTT 클라이언트 간의 메세지를 조정하는 클래스입니다. 
+ */
 public interface MQTTBroker {
     /**
      * MQTTCLient가 subscribe하도록 합니다.
