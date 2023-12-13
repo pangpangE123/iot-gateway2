5b159070d40a141924cf1de25b68d44d736d820a
Maru375 <www980304@gmail.com>
2023-12-13 19:33:19 +0900

    feat: NodeProperty 추가

---
+++ src/main/java/com/nhnacademy/aiot/node/NodeProperty.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.aiot.node;
+
+import org.json.JSONObject;
+
+public class NodeProperty extends JSONObject {
+
+    public NodeProperty(String string) {
+        super(string);
+    }
+}
