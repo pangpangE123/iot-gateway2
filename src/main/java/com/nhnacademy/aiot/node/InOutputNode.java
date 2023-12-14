4fee541cc3684ed8e6e40bc639a5ed9ec0ad357f
pangpangE123 <eric0420@naver.com>
2023-12-14 16:47:16 +0900

    feat : node & nodeTestcode
    - InputNode, OutputNode, InOutputNode, InputNodeTest, OutputNodeTest, InOutputNodeTest

--- src/main/java/com/nhnacademy/aiot/node/InOutputNode.java
+++ src/main/java/com/nhnacademy/aiot/node/InOutputNode.java
@@ -3,6 +3,7 @@ package com.nhnacademy.aiot.node;
 import java.util.HashMap;
 import java.util.Map;
 import com.nhnacademy.aiot.packet.Packet;
+import com.nhnacademy.aiot.packet.Port;
 import com.nhnacademy.aiot.packet.Wire;
 
 /**
@@ -12,36 +13,56 @@ import com.nhnacademy.aiot.packet.Wire;
  * <p>
  * InputNode와 OutputNode의 역할을 다 합니다
  */
-public class InOutputNode extends ActiveNode {
-
+public abstract class InOutputNode extends ActiveNode {
     private final Map<Integer, Port> outPorts = new HashMap<>();
-    private final Port inPort;
+    private final Port inPort = new Port();
 
+    private InOutputNode(int portNumber) {
+        super();
+        for (int i = 0; i < portNumber; i++) {
+            outPorts.put(i, new Port());
+        }
+    }
 
+    /**
+     * 어떤 포트의, 어떤 와이어와 연결해줄것인지 결정해주는 메서드 인풋
+     * 
+     * @param portNum
+     * @param outNode
+     */
     public void connect(int portNum, OutputNode outNode) {
-
         Wire connectWire = outPorts.get(portNum).add(); // 인노드의 아웃포트에 connectWire 달아줌
-
         outNode.connect(connectWire); // 아웃노드의 인포트에 connectWire 달아줌
 
     }
 
+    /**
+     * 이 노드가 가지고 있는 포트에, 특정 wire를 달아줍니다
+     * 
+     * @param wire
+     */
     protected void connect(Wire wire) {
-
-        inPort.add(wire);
+        // inPort.add(wire);
 
     }
 
+    /**
+     * 내가 지정한 포트로 패킷을 넘겨주는 메서드
+     * 
+     * @param portNum
+     * @param packet
+     */
     protected void send(int portNum, Packet packet) {
-
         if (outPorts.containsKey(portNum)) {
             outPorts.get(portNum).put(packet);
         }
 
     }
 
+    /**
+     * inPort에서 패킷을 꺼내옵니다
+     */
     protected Packet receive() {
-
         return inPort.take();
 
     }
---
+++ src/test/java/com/nhnacademy/aiot/node/InOutputNodeTest.java
@@ -0,0 +1,129 @@
+package com.nhnacademy.aiot.node;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import com.nhnacademy.aiot.packet.Packet;
+import com.nhnacademy.aiot.packet.Wire;
+
+public class InOutputNodeTest {
+
+    public class InputNodeImple extends InputNode {
+        protected InputNodeImple(NodeProperty nodeProperty) {
+            super(nodeProperty);
+        }
+
+        @Override
+        protected void process() {
+            // ignore
+        }
+    }
+
+    public class OutputNodeImple extends OutputNode {
+        protected OutputNodeImple() {
+            super();
+        }
+
+        @Override
+        protected void process() {
+            // ignore
+        }
+    }
+
+    InputNode inputNode;
+    OutputNode outputNode;
+
+
+
+    @BeforeEach
+    void reset() {
+        NodeProperty mock = mock(NodeProperty.class);
+        when(mock.getInt("portNumber")).thenReturn(1);
+
+        inputNode = new InputNodeImple(mock);
+        outputNode = new OutputNodeImple();
+
+    }
+
+    @DisplayName("connect : 포트가 음수면 예외 발생")
+    @Test
+    void throwExceptionIfPortNumberIsMinus() {
+        // given
+        OutputNode mock = mock(OutputNode.class);
+
+        // when
+        Executable executable = () -> inputNode.connect(-1, mock);
+
+        // then
+        assertThrows(IllegalArgumentException.class, executable);
+    }
+
+
+    @DisplayName("connect : outNode가 null이면 예외 발생")
+    @Test
+    void throwExceptionIfOutNodeIsNull() {
+        // given
+        OutputNode mock = null;
+
+        // when
+        Executable executable = () -> inputNode.connect(1, mock);
+
+        // then
+        assertThrows(NullPointerException.class, executable);
+
+    }
+
+    @DisplayName("send : 포트가 음수면 예외 발생 ")
+    @Test
+    void throwExceptionIfPortNumberIsMinus2() {
+        // given
+        Packet packet = new Packet();
+        // when
+        Executable executable = () -> inputNode.send(-1, packet);
+        // then
+        assertThrows(IllegalArgumentException.class, executable);
+
+    }
+
+    @DisplayName("send : InputNode의 outPorts Map에서 입력한 포트넘버에 실제 outPort가 없을경우")
+    @Test
+    void throwExceptionIfWhereIsMyOutport() {
+        // given
+        Packet packet = new Packet();
+
+        // when
+        Executable executable = () -> inputNode.send(4, packet);
+
+        // then
+        assertThrows(NullPointerException.class, executable);
+    }
+
+    @DisplayName("send : Packet이 없으면 예외 발생")
+    @Test
+    void throwExceptionIfWhereIsMyPacket() {
+        // given
+        Packet packet = null;
+
+        // when
+        Executable executable = () -> inputNode.send(0, packet);
+
+        // then
+        assertThrows(NullPointerException.class, executable);
+
+    }
+
+    @DisplayName("연결하려고 하는 wire가 없을때")
+    @Test
+    void throwExceptionIfWhereIsWire() {
+        // given
+        Wire wire = null;
+        // when
+        Executable executable = () -> outputNode.connect(wire);
+        // then
+        assertThrows(NullPointerException.class, executable);
+    }
+}
